329fe2d76508ed11559584ddbae4477ad9ffa155
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-16 23:20:55 +0300

    Dicey App: Initial Commit

---
+++ Dicey/Dicey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicey
+//
+//  Created by Arthur Kho on 16/06/2018.
+//  Copyright © 2018 Arthur Kho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
