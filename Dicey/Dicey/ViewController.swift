816b0279e884ae92d4fde5015be031b5c138e0a8
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-17 11:55:15 +0300

    Creating randomisation in Swift 4.1

--- Dicey/Dicey/ViewController.swift
+++ Dicey/Dicey/ViewController.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+
+  var randomDice1 = 0
+  var randomDice2 = 0
 
   @IBOutlet weak var dice1: UIImageView!
   @IBOutlet weak var dice2: UIImageView!
 
   @IBAction func buttonPressed(_ sender: UIButton) {
+    randomDice1 = Int(arc4random_uniform(6))
+    randomDice2 = Int(arc4random_uniform(6))
+
+    print(randomDice1)
   }
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
