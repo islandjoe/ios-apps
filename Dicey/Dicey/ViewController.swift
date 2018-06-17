406493904aa1a3414c44bf41dcf9d325b3eb7ab6
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-17 12:43:01 +0300

    Programmatically update the display

--- Dicey/Dicey/ViewController.swift
+++ Dicey/Dicey/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
   var randomDice1 = 0
   var randomDice2 = 0
 
-  @IBOutlet weak var dice1: UIImageView!
-  @IBOutlet weak var dice2: UIImageView!
+  let diceFaces = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
   @IBOutlet weak var diceImg1: UIImageView!
   @IBOutlet weak var diceImg2: UIImageView!
 
@@ -23,7 +23,8 @@ class ViewController: UIViewController {
     randomDice1 = Int(arc4random_uniform(6))
     randomDice2 = Int(arc4random_uniform(6))
 
-    print(randomDice1)
+    diceImg1.image = UIImage(named: diceFaces[randomDice1])
+    diceImg2.image = UIImage(named: diceFaces[randomDice2])
   }
 
   override func viewDidLoad() {
