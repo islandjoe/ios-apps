45e57076a5b350634f1e0fbaff5747fa91f78b71
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-23 07:49:56 +0300

    Calculator on Stack View

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Arthur Kho on 23/06/2018.
+//  Copyright © 2018 Arthur Kho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
